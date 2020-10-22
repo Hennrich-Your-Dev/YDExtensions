6a11275ae4e6be67e038b99094026856ca3ece11
Douglas Hennrich <douglashennrich@yourdev.com.br>
2020-10-22 11:44:15 -0300

    First commit

---
+++ YDExtensions/UIKit/Color/UIColorExtension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColorExtension.swift
+//  YDExtensions
+//
+//  Created by Douglas Hennrich on 22/10/20.
+//
+
+import UIKit
+
+extension UIColor {
+  static let customRed = UIColor(red: 230/255, green: 0/255, blue: 20/255, alpha: 1)
+}
