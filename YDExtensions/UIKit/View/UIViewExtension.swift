2e6649520313054e7f18a73115ad7c232b51a414
Douglas Hennrich <douglashennrich@yourdev.com.br>
2020-10-26 21:09:19 -0300

    Adding UIView roundCorners

--- YDExtensions/UIKit/View/UIViewExtension.swift
+++ YDExtensions/UIKit/View/UIViewExtension.swift
@@ -66,4 +66,17 @@ extension UIView {
       }
     }
   }
+
+  // MARK: Corner round
+  func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+    let path = UIBezierPath(
+      roundedRect: bounds,
+      byRoundingCorners: corners,
+      cornerRadii: CGSize(width: radius, height: radius)
+    )
+    
+    let mask = CAShapeLayer()
+    mask.path = path.cgPath
+    layer.mask = mask
+  }
 }
